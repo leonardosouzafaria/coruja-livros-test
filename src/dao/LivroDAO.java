4bb96ba5ec7b9d6d5c8d2829a1bf70f14756f2bf
Renan Viana <renanvianalc@gmail.com>
2018-09-01 16:47:02 -0300

    Classe de acesso ao banco de dados

--- src/dao/LivroDAO.java
+++ src/dao/LivroDAO.java
@@ -30,8 +30,8 @@ public class LivroDAO {
 		}
 	}
 	//delete
-	public boolean delete() {
-		String sql = "";
+	public boolean delete(Livro livro) {
+		String sql = "DELETE FROM Livro WHERE codigoLivro = ?";
 		try(Connection conn = ConnectionFactory.abrirConexao();
 				PreparedStatement stm = conn.prepareStatement(sql)) {
 			
@@ -39,10 +39,21 @@ public class LivroDAO {
 			e.printStackTrace();
 		}
 		return false;
+	}	
+	//carregar registro único
+	public Livro carregar(Livro livro) {
+		String sql = "SELECT * FROM Livro WHERE codigoLivro = ?";
+		try(Connection conn = ConnectionFactory.abrirConexao();
+				PreparedStatement stm = conn.prepareStatement(sql)) {
+			stm.setInt(1, livro.getCodigoLivro());
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return livro;
 	}
-	//carregar
-	public Livro carregar(int codigoLivro) {
-		String sql = "";
+	//carregar todos os registros
+	public Livro carregar() {
+		String sql = "SELECT * FROM Livro";
 		Livro livro = null;
 		try(Connection conn = ConnectionFactory.abrirConexao();
 				PreparedStatement stm = conn.prepareStatement(sql)) {
