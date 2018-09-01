0de7f53bcfde333d68981ac747cbe0cc40f8f816
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-01 15:08:52 -0300

    Criação da classe Autor

---
+++ src/model/Autor.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Autor {
+	private int codigoAutor;
+	private String nome;
+	
+	//Contrutor completo autor
+	public Autor(int codigoAutor, String nome) {
+		
+	}
+	
+	
+	//Metodos de acesso Get/Set
+	public int getCodigoAutor() {
+		return codigoAutor;
+	}
+	
+	
+	
+}
--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,19 +1,23 @@
 package model;
 
 public class Livro {
-	//atributos
+	// atributos
 	private int codigoLivro;
 	private int codigoTema;
 	private int codigoAutor;
 	private String titulo;
 	private int quantidadePaginas;
-	//construtor padrão
-	public Livro() { }
-	//construtor apenas com id
+
+	// construtor padrão
+		//public Livro() {
+	//}
+
+	// construtor apenas com id
 	public Livro(int codigoLivro) {
 		setCodigoLivro(codigoLivro);
 	}
-	//construtor completo
+
+	// construtor completo
 	public Livro(int codigoLivro, int codigoTema, int codigoAutor, String titulo, int quantidadePaginas) {
 		setCodigoLivro(codigoLivro);
 		setCodigoTema(codigoTema);
@@ -21,34 +25,44 @@ public class Livro {
 		setTitulo(titulo);
 		setQuantidadePaginas(quantidadePaginas);
 	}
-	//métodos de acesso e métodos modificadores
+
+	// métodos de acesso e métodos modificadores
 	public int getCodigoLivro() {
 		return codigoLivro;
 	}
+
 	public void setCodigoLivro(int codigoLivro) {
 		this.codigoLivro = codigoLivro;
 	}
+
 	public int getCodigoTema() {
 		return codigoTema;
 	}
+
 	public void setCodigoTema(int codigoTema) {
 		this.codigoTema = codigoTema;
 	}
+
 	public int getCodigoAutor() {
 		return codigoAutor;
 	}
+
 	public void setCodigoAutor(int codigoAutor) {
 		this.codigoAutor = codigoAutor;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public int getQuantidadePaginas() {
 		return quantidadePaginas;
 	}
+
 	public void setQuantidadePaginas(int quantidadePaginas) {
 		this.quantidadePaginas = quantidadePaginas;
 	}
